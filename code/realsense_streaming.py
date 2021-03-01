6e007486eb9b18a0ebfd4864a7ffa85d3f09409e
thanthamky <thanthamky@gmail.com>
2021-03-01 21:37:11 +0700

    Update realsense_streaming.py

--- code/realsense_streaming.py
+++ code/realsense_streaming.py
@@ -1,70 +1,71 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
-
-ply_file_save = ""
+from open3d import *
+import datetime
+# from PIL import Image
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-    
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
-
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
+align = rs.align(rs.stream.color)
 
-try:
-    while True:
+vis = visualization.Visualizer()
+vis.create_window('PCD', width=1280, height=720)
+pointcloud = geometry.PointCloud()
+geom_added = False
+    
+while True:
+    dt0=datetime.datetime.now()
+    frames = pipeline.wait_for_frames()
+    frames = align.process(frames)
+    profile = frames.get_profile()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+    if not depth_frame or not color_frame:
+        continue
+    
+    # Convert images to numpy arrays
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+    
+    img_depth = geometry.Image(depth_image)
+    img_color = geometry.Image(color_image)
+    
+    rgbd = geometry.RGBDImage.create_from_color_and_depth(img_color, img_depth, convert_rgb_to_intensity=False)
+    
+    intrinsics = profile.as_video_stream_profile().get_intrinsics()
+    pinhole_camera_intrinsic = camera.PinholeCameraIntrinsic(intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy)
+   
+    pcd = geometry.PointCloud.create_from_rgbd_image(rgbd, pinhole_camera_intrinsic)
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    pointcloud.points = pcd.points
+    pointcloud.colors = pcd.colors
+    
+    if geom_added == False:
+        vis.add_geometry(pointcloud)
+        geom_added = True
+    
+    vis.update_geometry(pointcloud)
+    vis.poll_events()
+    vis.update_renderer()
+    
+    cv2.imshow('bgr', color_image)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
-
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
-
-        # Show images
-        cv2.namedWindow('RealSense D435i Streaming', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        
-        # Press C to save ply, Press Q to exit
-        if cv2.waitKey(1) & 0xFF == ord('c'):
-            ply = rs.save_to_ply(ply_file_save)
-            ply.set_option(rs.save_to_ply.option_ply_binary, False)
-            ply.set_option(rs.save_to_ply.option_ply_normals, True)
-            print('Saving ply to: ', ply_file_save)
-            ply.process(depth_colormap)
-            
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    process_time = datetime.datetime.now() - dt0
+    print("FPS: "+str(1/process_time.total_seconds()))
+    
     
 
-finally:
-
-    # Stop streaming
-    pipeline.stop()
-    print('Exit streaming')
+pipeline.stop()
+cv2.destroyAllWindows()
+vis.destroy_window()
+# del vis``
